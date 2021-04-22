2895f083a7607ed488ee92d856d6c9f9e4bfa2d0
Adam Gray <a.gray.4797@gmail.com>
2021-04-22 15:22:41 +0100

    First task and factorial challenge added

---
+++ age.py
@@ -0,0 +1,4 @@
+u_name = str(input("What is your name? "))
+u_age = int(input("What is your age? "))
+print(f"Hi {u_name}, you will turn 100 in {2021-u_age+100}")
+
---
+++ fact.py
@@ -0,0 +1,24 @@
+import numpy as np
+n_type = str(input("Is your number a float or an integer? "))
+if n_type == "int":
+    x = int(input("What is your integer? "))
+
+    def fact(n):
+        if n == 1:
+            return 1
+        return n*fact(n-1)
+    print(f"x factorial = {fact(x)}")
+else:
+    x = float(input("What is your float? "))
+
+    def gamma(n):
+        l = []
+        for i in range(1, 100000):
+            a = (1/(1+(n/i)))*np.exp(n/i)
+            l.append(a)
+        b = (1/n)*np.exp(-0.5772*n)
+        return b*(np.prod(l))
+    print(f"factorial of x is {gamma(x+1)}")
+
+
+
