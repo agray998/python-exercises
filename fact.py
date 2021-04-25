2d9df91a3dd72438850a57ef8f4d851164b8ca3e
Adam Gray <a.gray.4797@gmail.com>
2021-04-25 14:26:56 +0100

    Improved fact.py

--- fact.py
+++ fact.py
@@ -1,21 +1,23 @@
 import numpy as np
-n_type = str(input("Is your number a float or an integer? "))
-if n_type == "int":
+def fact(n):
+    if n == 0:
+        return 1
+    return n*fact(n-1)
+
+def gamma(n):
+    p = 1
+    for i in range(1, 100000):
+        a = (1/(1+(n/i)))*np.exp(n/i)
+        p *= a
+    b = (1/n)*np.exp(-0.5772*n)
+    return b*p
+
+n_type = str(input("Is your number a float (f) or an integer (i)? "))
+if n_type in {'i', 'int', 'integer'}:
     x = int(input("What is your integer? "))
-
-    def fact(n):
-        if n == 1:
-            return 1
-        return n*fact(n-1)
-    print(f"x factorial = {fact(x)}")
-else:
+    print(f"{x} factorial = {fact(x)}")
+elif n_type in {'f', 'float'}:
     x = float(input("What is your float? "))
-
-    def gamma(n):
-        p = 1
-        for i in range(1, 100000):
-            a = (1/(1+(n/i)))*np.exp(n/i)
-            p *= a
-        b = (1/n)*np.exp(-0.5772*n)
-        return b*p
-    print(f"factorial of x is {gamma(x+1)}")
+    print(f"{x} factorial is {gamma(x+1)}")
+else:
+    print(f"Input of type {n_type} is not supported")
