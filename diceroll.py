d1dfb72029c51fa878d0c012c56dc0eb0af55424
Adam Gray <a.gray.4797@gmail.com>
2021-04-26 14:43:57 +0100

    refactored diceroll and updated README

--- diceroll.py
+++ diceroll.py
@@ -1,5 +1,2 @@
 import dice # Importing dice.py as a module
-i = 0 # Initialising increment count
-while i<2: # Defining loop to perform two rolls
-    print(dice.roll()) # Calling roll function from dice
-    i+=1 # Incrementing counter
+print ((dice.roll(), dice.roll()))
