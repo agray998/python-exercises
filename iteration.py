47d2d4e4c0378a10c4369c47e4762ef9a5028525
Adam Gray <a.gray.4797@gmail.com>
2021-04-26 14:52:50 +0100

    refactored iteration.py

--- iteration.py
+++ iteration.py
@@ -1,5 +1,9 @@
+def isawesome(name):
+    string = f"{name} is awesome!"
+    return string
+
 i = 0
 while i<5:
     u_name = str(input("What is your name? "))
-    print(f"{u_name} is awesome!)
+    print(isawesome(u_name))
     i+=1
