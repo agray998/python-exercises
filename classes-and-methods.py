016f5a670af5f5c8990bd26760a5064b3d41a8e7
Adam Gray <a.gray.4797@gmail.com>
2021-04-27 12:02:31 +0100

    refactored classes and methods

--- classes-and-methods.py
+++ classes-and-methods.py
@@ -2,15 +2,11 @@ class Student:
 
     default = "student"
 
-    def __init__(self):
-        self.name = Student.default
-        self.age = Student.default
-        self.class_ = Student.default
-
-    def set_info(self, name, age, class_):
+    def __init__(self, name, age, class_=default):
         self.name = name
         self.age = age
         self.class_ = class_
+
 
     def grade_calc(self, h, a, f):
         self.percent = (h + a + f)*100/175
@@ -18,16 +14,12 @@ class Student:
         for x in bound:
             if self.percent > x:
                 self.grade = bound[x]
-                return f"You scored {self.percent}%. Your grade is {self.grade}"
+                return f"{self.name} scored {self.percent}%. {self.name}'s grade is {self.grade}"
                 break
             else:
                 continue
         else:
-            return f"You scored {self.percent}%. Sorry, you did not pass"
+            return f"{self.name} scored {self.percent}%. Sorry, you did not pass"
 
-
-john = Student()
-print(john.name)
-john.set_info('John', 23, 'Bio')
-print(john.name)
+john = Student('John', 23)
 print(john.grade_calc(20, 40, 78))
